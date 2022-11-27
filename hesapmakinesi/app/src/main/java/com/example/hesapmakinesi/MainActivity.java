c08ca4677b95fe1c69d7fc14fa08d1e3a1439ff0
bcilak <bcilak@gmail.com>
2022-11-27 21:26:56 +0300

    CalculatorMobileApp

---
+++ hesapmakinesi/app/src/main/java/com/example/hesapmakinesi/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.hesapmakinesi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    String eskisayi="";
+    String op ="+";
+    boolean yeniop=true;
+    EditText editText;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.ekran);
+    }
+    public void numberEvent(View view){
+        if(yeniop)
+            editText.setText("");
+            yeniop=false;
+        String number= editText.getText().toString();
+        switch (view.getId()){
+            case R.id.button:
+            number+="1";
+            break;
+
+            case R.id.button2:
+                number+="2";
+                break;
+            case R.id.button3:
+                number+="3";
+                break;
+            case R.id.button4:
+                number+="4";
+                break;
+            case R.id.button5:
+                number+="5";
+                break;
+            case R.id.button6:
+                number+="6";
+                break;
+            case R.id.button7:
+                number+="7";
+                break;
+            case R.id.button8:
+                number+="8";
+                break;
+            case R.id.button9:
+                number+="9";
+                break;
+            case R.id.button10:
+                number+="0";
+                break;
+            case R.id.button24:
+                number+=".";
+                break;
+            case R.id.button16:
+                number+="-"+number;
+                break;
+        }
+        editText.setText(number);
+    }
+    public void operatorEvent (View view){
+        yeniop=true;
+        eskisayi=editText.getText().toString();
+        switch (view.getId()){
+            case R.id.button17: op ="/";break;
+            case R.id.button19: op ="*";break;
+            case R.id.button21: op ="+";break;
+            case R.id.button22: op ="-";break;
+            case R.id.button11: op ="sin";break;
+            case R.id.button12: op ="cos";break;
+            case R.id.button13: op ="tan";break;
+            case R.id.button14: op ="!";break;
+
+        }
+    }
+    public void equalEvent(View view){
+        String yenisayi =editText.getText().toString();
+        double sonuc =0.0;
+        switch (op){
+            case "+":
+                sonuc = Double.parseDouble(eskisayi)+Double.parseDouble(yenisayi);
+                break;
+            case "-":
+                sonuc = Double.parseDouble(eskisayi)-Double.parseDouble(yenisayi);
+                break;
+            case "*":
+                sonuc = Double.parseDouble(eskisayi)*Double.parseDouble(yenisayi);
+                break;
+            case "/":
+                sonuc = Double.parseDouble(eskisayi)/Double.parseDouble(yenisayi);
+                break;
+            case "sin":
+                sonuc = Math.sin(Double.parseDouble(eskisayi));
+                break;
+            case "cos":
+                sonuc = Math.cos(Double.parseDouble(eskisayi));
+                break;
+            case "tan":
+                sonuc = Math.tan(Double.parseDouble(eskisayi));
+                break;
+            case "!":
+                sonuc = factorial(Integer.parseInt(eskisayi));
+                break;
+
+
+
+        }
+        editText.setText(sonuc+"");
+    }
+    int factorial(int n)
+    {
+        return (n==1 || n==0) ? 1 : n*factorial(n-1);
+    }
+    public void acEvent(View view ){
+        editText.setText("0");
+        yeniop=true;
+    }
+    public void percentEvent (View view){
+        double no =Double.parseDouble(editText.getText().toString())/100;
+        editText.setText(no+"");
+        yeniop=true;
+    }
+}
